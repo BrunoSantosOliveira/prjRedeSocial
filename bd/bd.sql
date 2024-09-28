bed9e0618672875cd1e4d137d7d530c2fdf1518a
Bruno dos Santos Oliveira <brunoso2006@gmail.com>
2024-09-27 22:58:53 -0300

    Criando Código MySQL
    Criação da Primeira TABLE

---
+++ bd/bd.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE cozinhaDescomplica;
+USE cozinhaDescomplica;
+
+CREATE TABLE tb_users(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR() NOT NULL UNIQUE,
+    nomeCompleto VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    senha VARCHAR(300) NOT NULL,
+    fotoPerfil BLOB NOT NULL
+);
