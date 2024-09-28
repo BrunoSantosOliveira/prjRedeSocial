3080622bcd57fb7869733f52cdabd849c7e19408
iigguu <100096933+iigguu@users.noreply.github.com>
2024-09-27 22:23:00 -0300

    27/09

--- index.php
+++ index.php
@@ -3,10 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>REDE SOCIAL</title>
+    <title>Cozinha Descomplica</title>
     <link rel="stylesheet" href="/styles/style.css">
 </head>
 <body>
-    
+    <header>
+        <div class="logo">
+            <h2>Finexo.</h2>
+        </div>
+        <nav class="navbar">
+            <a class="navbaritem" href="home/home.php#slider_section">Início</a>
+            <a class="navbaritem" href="home/home.php#service_section">Serviços</a>
+            <a class="navbaritem" href="home/home.php#about_section">Sobre Nós</a>
+        </nav>
+    </header>
 </body>
 </html>
